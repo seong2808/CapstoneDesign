cec79e978ce492f8e9e460708fd8cfe347ab9ee9
seong2808 <seong2808@naver.com>
2022-10-24 17:00:20 +0900

    10_24 commit

--- src/App.js
+++ src/App.js
@@ -1,93 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './main1.css';
-import './start_style.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./banner.css";
+import "./start_style.css";
 
 function App() {
+  let posts = "";
 
-  let posts = '강남 고기 맛집';
+  let oh = "";
 
-  let oh = '오익준 넷플 시청예정';
-  
   function asd() {
-    return 100
+    return 100;
   }
-  
+
   return (
     <div className="App">
-      <div class="con1">
-
+      <div className="con1">
         <img id="logo" src="./logo.png" />
-      
-        <div class="con2">
-          <nav id="main_menu" class="main_menu">
-            
-                <a href="">소개 및 사용법</a>
-              
-                <a href="">검색</a>
-              
-                <a href="">안전수칙</a>
-            
-                <a href="">자전거 정책</a>
-             
-                <a href="">코로나19</a>
-            
-          </nav>
-        </div>
-        <div className='more-btn'>
-          <i className='icon'></i>
-        </div>
 
+        <nav id="main_menu" className="main_menu">
+          <a href="#">소개 및 사용법</a>
+          <a href="#">자전거 정책</a>
+          <a href="#">자전거 안전교육</a>
+          <a href="#">검색</a>
+        </nav>
+
+        <div className="more-btn">
+          <i className="icon"></i>
+        </div>
       </div>
-      <div class="ride">
-        <img src="./ride.jpg" width="100%" height="300px"/>
+      <div className="ride">
+        <img src="./rideback.png" width="100%" height="100%" />
       </div>
 
-      <div class="section">
-        <input type="radio" name="slide" id="slide01" checked/>
-        <input type="radio" name="slide" id="slide02"/>
-        <input type="radio" name="slide" id="slide03"/>
+      <div className="section">
+        <input type="radio" name="slide" id="slide01" checked />
+        <input type="radio" name="slide" id="slide02" />
+        <input type="radio" name="slide" id="slide03" />
         <div class="slidewrap">
-
-          <ul class="slidelist">
+          <ul className="slidelist">
             <li>
               <a>
-                <img src="./black.jpg"/>
+                <img src="./black.jpg" />
               </a>
             </li>
             <li>
               <a>
-                <img src="./red.jpg"/>
+                <img src="./red.jpg" />
               </a>
             </li>
             <li>
               <a>
-                <img src="./puple.jpg"/>
+                <img src="./puple.jpg" />
               </a>
             </li>
           </ul>
 
-          <div class="slide-control">
-            
-            <div class="control01">
+          <div className="slide-control">
+            <div className="control01">
               <label for="slide03" class="left"></label>
               <label for="slide02" class="right"></label>
             </div>
 
-            <div class="control02">
+            <div className="control02">
               <label for="slide01" class="left"></label>
               <label for="slide03" class="right"></label>
             </div>
 
-            <div class="control03">
+            <div className="control03">
               <label for="slide02" class="left"></label>
               <label for="slide01" class="right"></label>
             </div>
-        
           </div>
         </div>
       </div>
+
+      <div className="con2">
+        빈 공간
+      </div>
+
+      <div className="con3">
+        빈 공간
+      </div>
+
+      <div className="con4">
+        빈 공간
+      </div>
+
+      <div className="con5">
+        빈 공간
+      </div>
+    
+    <hr></hr>
+    <div className="footer">
+      빈 공간
+    </div>
 
     </div>
   );
