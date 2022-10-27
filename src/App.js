341217ed82c5aae2202d5b3c25e76fabcb265927
seong2808 <seong2808@naver.com>
2022-10-27 17:28:29 +0900

    10_27 commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,44 @@
+
 import React from "react";
 import "./App.css";
 import "./banner.css";
 import "./start_style.css";
 import Main1 from "./component/Main1";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import Intro from "./component/Intro";
 
 function App() {
-  
   return (
-    <div className="App">
-      <div className="con1">
-        <img id="logo" src="./logo.png" />
+    <BrowserRouter>
+    
+      <div className="App">
+        <div className="con1">
+          <img id="logo" src="./logo.png" >
+          </img>
+          <nav id="main_menu" className="main_menu">
+            <Link to="/Intro">소개 및 사용법</Link>
+            <Link to="/ro">자전거 정책</Link>
+            <Link to="/edu">자전거 안전교육</Link>
+            <Link to="/sear">검색</Link>
+          </nav>
 
-        <nav id="main_menu" className="main_menu">
-          <a href="#">소개 및 사용법</a>
-          <a href="#">자전거 정책</a>
-          <a href="#">자전거 안전교육</a>
-          <a href="#">검색</a>
-        </nav>
-
-        <div className="more-btn">
-          <i className="icon"></i>
+          <div className="more-btn">
+            <i className="icon"></i>
+          </div>
         </div>
-      </div> 
-      <Main1 />
-      <hr></hr>
-      <div className="footer">
-        빈 공간
+        <Switch>
+          <Route exact path="/">
+            <Main1 />
+          </Route>
+          <Route path="/Intro">
+            <Intro />
+          </Route>
+        </Switch>
+
+        <hr></hr>
+        <div className="footer">빈 공간</div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
